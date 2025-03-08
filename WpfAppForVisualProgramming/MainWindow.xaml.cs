8bb086dc8c4f5b558d872176bc9cb1c0a34ce20e
Богдан Безуглый <winzar06031977@gmail.com>
2025-03-08 15:57:35 +0200

    Create grid in window

--- WpfAppForVisualProgramming/MainWindow.xaml.cs
+++ WpfAppForVisualProgramming/MainWindow.xaml.cs
@@ -20,10 +20,39 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        Ellipse el = new Ellipse();
-        el.Width = 200;
-        el.Height = 200;
-        el.Fill = Brushes.Aqua;
-        grid1.Children.Add(el);
+        Line vertLine = new Line();
+        vertLine.Stroke = Brushes.Black;
+        vertLine.X1 = 10;
+        vertLine.Y1 = 0;
+        vertLine.X2 = 10;
+        vertLine.Y2 = 450;
+        Line horizLine = new Line();
+        horizLine.Stroke = Brushes.Black;
+        horizLine.X1 = 0;
+        horizLine.Y1 = 10;
+        horizLine.X2 = 800;
+        horizLine.Y2 = 10;
+        grid1.Children.Add(vertLine);
+        grid1.Children.Add(horizLine);
+        for (int i = 20; i < 800; i+=10)
+        {
+            Line a = new Line();
+            a.Stroke = Brushes.Black;
+            a.X1 += i;
+            a.X2 += i;
+            a.Y1 = vertLine.Y1;
+            a.Y2 = vertLine.Y2;
+            grid1.Children.Add(a);
+        }
+        for (int i = 20; i < 450; i+=10)
+        {
+            Line a = new Line(); 
+            a.Stroke = Brushes.Black;
+            a.Y1 += i;
+            a.Y2 += i;
+            a.X1 = horizLine.X1;
+            a.X2 = horizLine.X2;
+            grid1.Children.Add(a);
+        }
     }
 }
